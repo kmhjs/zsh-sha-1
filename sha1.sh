b5caeb88556d1bbd18387159432960b753a3a4fc
K. Murakami <kmhjs@users.noreply.github.com>
2016-01-30 05:29:23 +0900

    [ADD] Added binary split function

--- sha1.sh
+++ sha1.sh
@@ -4,12 +4,19 @@ function sha1::integer::fromChar()
     printf "%d" \'${1}
 }
 
+function sha1::binary::fromInteger()
+{
+    local input_value=${1}
+
+    echo "ibase=10;obase=2;${input_value}" | bc
+}
+
 function sha1::binary::fromChar()
 {
     # This function converts input ascii char into 8-bits binary form
     local input_char=${1}
     local ascii_value=$(sha1::integer::fromChar ${input_char})
-    local binary_string=$(echo "ibase=10;obase=2;${ascii_value}" | bc)
+    local binary_string=$(sha1::binary::fromInteger ${ascii_value})
 
     [[ ${#binary_string} < 8 ]] && {
         local padding_length=$((8 - ${#binary_string}))
@@ -32,4 +39,59 @@ function sha1::binary::fromString()
     ; done
 
     echo -n ${binary_string}
+}
+
+function sha1::binary::appendOne()
+{
+    # This function appends 1 in the tail of input
+    local binary_string=${1}
+
+    echo -n "${binary_string}1"
+}
+
+function sha1::binary::splitIntoBitBlocks()
+{
+    local input_binary_string=${1}
+    local block_length=${2}
+    local delimiter=${3}
+
+    local result_string=''
+
+    while [[ ${#input_binary_string} > ${block_length} ]]; do
+        result_string="${result_string}${delimiter}${input_binary_string[1,${block_length}]}"
+        input_binary_string="${input_binary_string[$((${block_length} + 1)), -1]}"
+    ; done
+
+    [[ ${#input_binary_string} != 0 ]] && result_string="${result_string}${delimiter}${input_binary_string}"
+
+    echo -n ${result_string[2, -1]}
+}
+
+function sha1::binary::zeroPadding()
+{
+    local base_length=512
+    local reserved_length=64
+    local input_binary_string=${1}
+    local data_length=$((${base_length} - ${reserved_length}))
+    local blocks=($(sha1::binary::splitIntoBitBlocks ${input_binary_string} ${data_length} ' '))
+
+    local result_blocks=''
+    local delimiter=' '
+
+    for idx ({1..${#blocks}}); do
+        local block=${blocks[${idx}]}
+
+        [[ ${idx} == ${#blocks} ]] && {
+            local padding_length=$((${data_length} - ${#block}))
+            [[ $((${padding_length} - 8)) > 0 ]] && {
+                block="${block}10000000"
+                padding_length=$((${padding_length} - 8))
+            }
+            block="${block}${(l.${padding_length}..0.)}"
+        }
+
+        result_blocks="${result_blocks}${delimiter}${block}"
+    ; done
+
+    echo -n ${result_blocks[2, -1]}
 }
--- sha1.test.sh
+++ sha1.test.sh
@@ -9,6 +9,13 @@ function test::sha1::integer::fromChar()
     [[ ${result} == 97 ]] && return 0 || return 1
 }
 
+function test::sha1::binary::fromInteger()
+{
+    local result=$(sha1::binary::fromInteger 97)
+
+    [[ "${result}" == "1100001" ]] && return 0 || return 1
+}
+
 function test::sha1::binary::fromChar()
 {
     local result=$(sha1::binary::fromChar 'a')
@@ -23,15 +30,38 @@ function test::sha1::binary::fromString()
     [[ "${result}" == "0100100001100101011011000110110001101111001000000111011101101111011100100110110001100100" ]] && return 0 || return 1
 }
 
-function sha1.test
+function test::sha1::binary::appendOne()
 {
-    local test_file_name="${0}.sh"
+    local result=$(sha1::binary::appendOne '00000000')
 
-    foreach function_name ($(cat ${test_file_name} | sed -n 's!^function  *test!test!p' | tr -d '()')); do
-        ${function_name}
-
-        [[ ${?} != 0 ]] && {
-            echo "TEST FAILED: ${function_name} finished with invalid exit code." > /dev/stderr
-        }
-    ; done
+    [[ "${result}" == "000000001" ]] && return 0 || return 1
 }
+
+function test::sha1::binary::splitIntoBitBlocks()
+{
+    local block_length=16
+    local input_sample=${(l.33..0.)}
+    local delimiter=' '
+
+    local result=$(sha1::binary::splitIntoBitBlocks ${input_sample} ${block_length} ${delimiter})
+
+    [[ "${result}" == "${(l.16..0.)} ${(l.16..0.)} ${(l.1..0.)}" ]] && return 0 || return 1
+}
+
+function test::sha1::binary::zeroPadding()
+{
+    local data_length=$((512 - 64))
+    local input_sample=${(l.$((${data_length} + 1))..0.)}
+
+    local result=$(sha1::binary::zeroPadding ${input_sample})
+
+    [[ "${result}" == "${(l.${data_length}..0.)} 01${(l.$((${data_length} - 2))..0.)}" ]] && return 0 || return 1
+}
+
+foreach function_name ($(cat ${0} | sed -n 's!^function  *test!test!p' | tr -d '()')); do
+    ${function_name}
+
+    [[ ${?} != 0 ]] && {
+    echo "TEST FAILED: ${function_name} finished with invalid exit code." > /dev/stderr
+}
+; done
