8db58db022d778433c9d5310b8d3f6bdeaa9ac85
K. Murakami <kmhjs@users.noreply.github.com>
2016-01-31 14:31:07 +0900

    [MOD] Updated test runner

--- sha1.test.sh
+++ sha1.test.sh
@@ -60,16 +60,47 @@ function test::sha1::binary::mapping::to_sha1_hex()
 
 function test::sha1::binary::mapping::update_internal_states()
 {
+    return 1
 }
 
 function test::sha1::main()
 {
+    local input_message='A Test'
+    local expected_result='8F0C0855915633E4A7DE19468B3874C8901DF043'
+
+    local result=$(sha1::main ${input_message})
+
+    if [[ "${result}" != "${expected_result}" ]]; then
+        return 1
+    fi
+
+    return 0
 }
 
-foreach function_name ($(cat ${0} | sed -n 's!^function  *test!test!p' | tr -d '()')); do
-    ${function_name}
+function run_test()
+{
+    local self_file_name=${1}
 
-    if [[ ${?} != 0 ]]; then
-        echo "TEST FAILED: ${function_name} finished with invalid exit code." > /dev/stderr
+    local succeeded=0
+    local failed=0
+
+    foreach function_name ($(cat ${self_file_name} | sed -n 's!^function  *test!test!p' | tr -d '()')); do
+        ${function_name}
+    
+        if [[ ${?} != 0 ]]; then
+            echo -e "\e[1;31mFAIL\e[0m: ${function_name} finished with invalid exit code." > /dev/stderr
+            failed=$((++failed))
+        else
+            echo -e "\e[1;32mPASS\e[0m: ${function_name} passed the test." > /dev/stderr
+            succeeded=$((++succeeded++))
+        fi
+    ; done
+
+    echo ${(l.${COLUMNS}..-.)}
+    if [[ ${failed} != 0 ]]; then
+        echo -e "\e[1;31mFAILED\e[0m the test." > /dev/stderr
     fi
-; done
+    echo "REPORT: PASS (${succeeded}), FAIL (${failed})" > /dev/stderr
+}
+
+run_test ${0}
